2e460964f159b3fea7f8f2ac0b40065ab702ff31
thekasem <yindeekasem@gmail.com>
2016-01-18 20:00:36 +0700

    Mv8.9.2

--- src/main/java/com/actions/DashBoardAction.java
+++ src/main/java/com/actions/DashBoardAction.java
@@ -14,8 +14,20 @@ public class DashBoardAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = -146843680046469165L;
-	private static String DASHBOARD = "dashboard";
+	private static final String DASHBOARD = "dashboard";
+	private static final String BROWSERS = "browsers";
+	private static final String LOGIN = "login";
+	private static final String USER = "user";
+	private static final String TOPMODULE = "topmodule";
+	private static final String NEWUSER = "newuser";
+	private static final String CUSTOMERS = "customers";
+	private static final String TOPUSER = "topuser";
+	private static final String ARGONSITE = "argonsite";
+	private static final String VIEWAUM = "viewaum";
+	private static final String DISKUSAGE = "diskusage";
+	
 	private String userNameLogin;
+	private String date;
 
 	public void ContactController() {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
@@ -27,6 +39,54 @@ public class DashBoardAction extends ActionSupport {
 	public String showDashBoard() {
 		ContactController();
 		return DASHBOARD;
+	}
+    
+	public String browser(){
+		return BROWSERS;
+	}
+	
+	public String user(){
+		return USER;
+	}
+	
+	public String login(){
+		return LOGIN;
+	}
+	
+	public String topModule(){
+		return TOPMODULE;
+	}
+	
+	public String newUser(){
+		return NEWUSER;
+	}
+	
+	public String customer(){
+		return CUSTOMERS;
+	}
+	
+	public String topUser(){
+		return TOPUSER;
+	}
+	
+	public String argTimeOnSite(){
+		return ARGONSITE;
+	}
+	
+	public String viewAUM(){
+		return VIEWAUM;
+	}
+	
+	public String diskUsage(){
+		return DISKUSAGE;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
 	}
 
 	public String getUserNameLogin() {
